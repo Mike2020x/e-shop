ebd59bbc70310a896e45f83140c546728e756d6e
Mike2020x <62442141+Mike2020x@users.noreply.github.com>
2023-10-17 23:38:23 -0500

    add color select component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,37 @@
+import { products } from "@/utils/products";
+import Container from "./components/Container";
+import HomeBanner from "./components/HomeBanner";
+import { truncateText } from "@/utils/TruncateText";
+import ProductCard from "./components/products/ProductCard";
+
 export default function Home() {
-  return <h1>E-SHOP</h1>;
+  return (
+    <div className="p-8">
+      <Container>
+        <div>
+          <HomeBanner />
+          <div
+            className="
+          grid
+          grid-cols-2
+          sm:grid-cols-3
+          lg:grid-cols-4
+          xl:grid-cols-5
+          2xl:grid-cols-6
+          gap-8
+          "
+          >
+            {products.map((product: any) => {
+              return (
+                <ProductCard
+                  data={product}
+                  key={product.id}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
 }
